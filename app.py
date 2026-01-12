342edce488f0c1203d5619e98a4648a0ef2644f2
Aria Denny A <165958726+Arden25@users.noreply.github.com>
2026-01-13 04:36:50 +0700

    Tambah input batas integral

--- app.py
+++ app.py
@@ -4,3 +4,17 @@ st.title("Aplikasi Solusi Integral")
 st.title("Metode Pias Persegi Panjang")
 
 fungsi = st.text_input("Masukan fungsi f(x)", "x**2")
+
+st.subheader("Batas Integral")
+
+a = st.number_input(
+  "Batas bawah (a)",
+  value = 1.0,
+  step = 0.1
+)
+
+b = st.number_input(
+  "Batas atas (b)",
+  value = 1.0,
+  step = 0.1
+)
